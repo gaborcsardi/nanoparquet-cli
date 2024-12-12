cb7d9af0e6357968b4abd96990eaab84e0f49040
Gábor Csárdi <csardi.gabor@gmail.com>
2024-12-12 21:20:10 +0100

    Fix script on Windows

--- nanoparquet.cmd
+++ nanoparquet.cmd
@@ -1,2 +1,16 @@
-IMG=ghcr.io/gaborcsardi/nanoparquet-cli
-docker run -v `pwd -P`:`pwd` -w `pwd` ${IMG} "$@"
+: # Command line Docker app to query and transform Parquet files
+: # Requires Docker installed and on the PATH
+: # Start with --help for more information
+: # Or see docs at https://github.com/gaborcsardi/nanoparquet-cli
+
+: # This is for Unix
+:; exec docker run -v `pwd -P`:`pwd` -w `pwd` ghcr.io/gaborcsardi/nanoparquet-cli "$@"
+
+: # This is for Windows
+@ECHO OFF
+SET IMG=ghcr.io/gaborcsardi/nanoparquet-cli
+SET DRIVE=%CD:~0,1%
+SET UCD=%cd:~2%
+: # Docker needs / in paths
+SET UCD=%UCD:\=/%
+docker run -v /%DRIVE%/%UCD%:%UCD% -w %UCD% %IMG% %*
